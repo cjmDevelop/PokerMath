6c65bcf82110e4dd2f3b6ad2b26f01c9e2d9151f
cjmDev <manbodhclaude@yahoo.com>
2023-09-27 04:05:31 -0400

    suit symbols print out in random order

--- src/Card.java
+++ src/Card.java
@@ -1,79 +1,107 @@
-import java.util.Objects;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Card {
+/* Card State -------------------------------------------------------------------------------------*/
+    private Random random = new Random();
+    private String[] suitRepresentation;
 
-    private String suitRepresentation;
-    private String valueRepresentation;
-    private Integer value;
+    private Suit suitFullName;
+    private final String clubs = "\u2663";
+    private final String hearts = "\u2665";
+    private final String spades = "\u2660";
+    private final String diamonds = "\u2666";
 
+    private Integer numericValueController = 0;
+    private String[] valueRepresentation = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
 
-    public Card() {
+    public static void main(String[] args) {
+        Card card = new Card();
+        System.out.println(Arrays.toString(card.randomizeRepresentation(card)));
     }
 
-    public Card(Integer value, String suitRepresentation) {
+
+/* Card Behavior  ---------------------------------------------------------------------------------------------------*/
+    /** setRandomSuiteRepresentation(Card card)
+     * Randomizes card suits; using The Fisher-Yates
+     * shuffle algorithm. Time-complexity of O(n),
+     * space-complexity of O(1).
+     */ public String[] randomizeRepresentation(Card card) {
+       String[] suitStamped = {
+               card.getClubs(),
+               getDiamonds(),
+               getHearts(),
+               getSpades()
+       };
+       setSuitRepresentation(suitStamped); //Initialized suit
+
+       for (int i = getSuitRepresentation().length - 1; i >= 0; i--) { // 1.) Descending Order.
+           int randomIndex = random.nextInt(i + 1); // 2.) Generate random number 0 - i inclusive.
+           String randomSuit = getSuitRepresentation()[randomIndex]; // 3.) get String based on random index.
+           getSuitRepresentation()[randomIndex] = getSuitRepresentation()[i]; // 4. swapped random string to ith index.
+           getSuitRepresentation()[i] = randomSuit;// 5.) Place the suit that was originally at the ith position at the random index.
+       }
+        return suitRepresentation;
+     }
+
+    public void setSuitRepresentation(String[] suitRepresentation) {
         this.suitRepresentation = suitRepresentation;
-        this.value = value;
     }
 
-    public Card(String suitRepresentation, String valueRepresentation, Integer value) {
-        this.suitRepresentation = suitRepresentation;
-        this.valueRepresentation = valueRepresentation;
-        this.value= value;
+    /* Card Boiler-Plate-Code - getters & setters --------------------------------------------------------------------------------*/
+    public Suit getSuit() {
+        return suitFullName;
     }
 
-    public Card(Integer value, String suitRepresentation, String valueRepresentation) {
-        this.suitRepresentation = suitRepresentation;
-        this.valueRepresentation = valueRepresentation;
-        this.value= value;
+    public String getClubs() {
+        return clubs;
     }
 
-    public String getSuitRepresentation() {
+    public String getHearts() {
+        return hearts;
+    }
+
+    public String getSpades() {
+        return spades;
+    }
+
+    public String getDiamonds() {
+        return diamonds;
+    }
+
+
+    public Random getRandom() {
+        return random;
+    }
+
+    public void setRandom(Random random) {
+        this.random = random;
+    }
+
+
+    public String[] getSuitRepresentation() {
         return suitRepresentation;
     }
 
-    public void setSuitRepresentation(String suitRepresentation) {
-        this.suitRepresentation = suitRepresentation;
+
+
+    public Integer getNumericValueController() {
+        return numericValueController;
+    }
+
+    public void setNumericValueController(Integer numericValueController) {
+        this.numericValueController = numericValueController;
     }
 
 
-    public void setValueRepresentation(String valueRepresentation) {
-        this.valueRepresentation = valueRepresentation;
-    }
-
-    public Integer getValue() {
-        return value;
-    }
-
-    public void setValue(Integer value) {
-        this.value = value;
-    }
-
-    public String getValueRepresentation() {
+    public String[] getValueRepresentation() {
         return valueRepresentation;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Card card = (Card) o;
-        return Objects.equals(suitRepresentation, card.suitRepresentation) && Objects.equals(valueRepresentation, card.valueRepresentation) && Objects.equals(value, card.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(suitRepresentation, valueRepresentation, value);
+    public void setValueRepresentation(String[] valueRepresentation) {
+        this.valueRepresentation = valueRepresentation;
     }
 
 
-    @Override
-    public String toString() {
-//    String adjustRepresentation = valueRepresentation.length() == 1 ? " " + valueRepresentation : valueRepresentation;
-
-        return "|" +
-                valueRepresentation + " " +
-                suitRepresentation +
-                " | ";
-    }
 
 }
--- src/Dealer.java
+++ src/Dealer.java
@@ -2,66 +2,69 @@ import java.util.Arrays;
 
 public class Dealer {
 
-
-    private Integer cardValue = 0;
-    private Card[] suitOfThirteen = new Card[13];
-    private Card[][] fiftyTwoUnShuffled = new Card[4][13];
-    private String[] suitSymbol = {"♥", "♠", "♦", "♣"};
-    private String[] valueRepresentation = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-    private String hearts = "♥";
-    private String spades =   "♠";
-    private String diamonds = "♦";
-    private String clubs = "♣";
-
     public static void main(String[] args) {
-        Dealer dealer = new Dealer();
-        dealer.getNewPack();
-        dealer.showSuitOf(dealer.getDiamonds());
+        Card cardTest = new Card();
+
     }
 
-    public Card[][] getNewPack() {
-        for (int j = 0; j < suitSymbol.length; j++) {
-            String suit = suitSymbol[j];
-            for (int i = 0; i < 13; i++) {
-                cardValue = (i + 1) + 1;
-                Card card = new Card(cardValue, suit, valueRepresentation[i]);
-                fiftyTwoUnShuffled[j][i] = card;
-            }
-        }
-        return fiftyTwoUnShuffled;
-    }
-
-    public void showPack() {
-        for (Card[] c: this.fiftyTwoUnShuffled) {
-            System.out.println(Arrays.deepToString(c));
-        }
-    }
-
-    public void showSuitOf(String suit) {
-        for (int i = 0; i < this.fiftyTwoUnShuffled.length; i++) {
-            for(int j = 0; j < this.fiftyTwoUnShuffled[0].length; j++){
-                if(this.fiftyTwoUnShuffled[i][j].getSuitRepresentation().equals(suit)) {
-                    System.out.print(this.fiftyTwoUnShuffled[i][j]);
-                }
-            }
-        }
-    }
-
-    public String getHearts() {
-        return hearts;
-    }
-
-    public String getSpades() {
-        return spades;
-    }
-
-    public String getDiamonds() {
-        return diamonds;
-    }
-
-    public String getClubs() {
-        return clubs;
-    }
+//
+//    private Card[] suitOfThirteen = new Card[13];
+//    private Card[][] fiftyTwoUnShuffled = new Card[4][13];
+//
+//
+//
+//    public static void main(String[] args) {
+//        Dealer dealer = new Dealer();
+//        dealer.getNewPack();
+//        dealer.showPack();
+//        System.out.println(" ");
+//        dealer.showSuitOf(dealer.getDiamonds());
+//        System.out.println();
+//    }
+//
+//    public Card[][] getNewPack() {
+//        for (int j = 0; j < suitRepresentation.length; j++) {
+//            String suit = suitRepresentation[j];
+//            for (int i = 0; i < 13; i++) {
+//                cardValue = (i + 1) + 1;
+//                Card card = new Card(cardValue, suit, valueRepresentation[i]);
+//                fiftyTwoUnShuffled[j][i] = card;
+//            }
+//        }
+//        return fiftyTwoUnShuffled;
+//    }
+//
+//    public void showPack() {
+//        for (Card[] c: this.fiftyTwoUnShuffled) {
+//            System.out.println(Arrays.deepToString(c));
+//        }
+//    }
+//
+//    public void showSuitOf(String suit) {
+//        for (int i = 0; i < this.fiftyTwoUnShuffled.length; i++) {
+//            for(int j = 0; j < this.fiftyTwoUnShuffled[0].length; j++){
+//                if(this.fiftyTwoUnShuffled[i][j].getSuitRepresentation().equals(suit)) {
+//                    System.out.print(this.fiftyTwoUnShuffled[i][j]);
+//                }
+//            }
+//        }
+//    }
+//
+//    public String getHearts() {
+//        return hearts;
+//    }
+//
+//    public String getSpades() {
+//        return spades;
+//    }
+//
+//    public String getDiamonds() {
+//        return diamonds;
+//    }
+//
+//    public String getClubs() {
+//        return clubs;
+//    }
 }
 
 
